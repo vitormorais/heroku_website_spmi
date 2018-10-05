9a0f6ecc162e5f5eeb6c0ea3dacda728960058d0
Unknown <vitor.fa.morais@gmail.com>
2018-10-05 16:03:42 +0100

    updated_apk+app

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
 	header('Location: pages/frontpage/frontpage.php'); 
+	header('Location: http://guia-spmi.herokuapp.com/pages/frontpage/frontpage.php');
 	exit();
 /*1º TESTE
 include_once('config/init.php');
--- pages/app/js/app.js
+++ pages/app/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app_spmi = angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
+app_spmi.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -21,7 +21,34 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+
+  // Override the android back button
+  $ionicPlatform.registerBackButtonAction(function (e) {
+        
+             if ($rootScope.$viewHistory.backView) {
+                $rootScope.$viewHistory.backView.go();
+            } else {
+                var confirmPopup = $ionicPopup.confirm({
+                        title: 'Confirm Exit',
+                        template: "Are you sure you want to close APPNAME?"
+                    });
+                confirmPopup.then(function (close) {
+                    if (close) {
+                        // there is no back view, so close the app instead
+                        ionic.Platform.exitApp();
+                    } // otherwise do nothing
+                    console.log("User canceled exit.");
+                });
+            }
+
+            e.preventDefault();
+            return false;
+        }, 101); // 1 more priority than back button
 })
+
+
+
+
 
 function PULMONAR_function() {
     
